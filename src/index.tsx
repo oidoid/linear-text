082c0ec4a0464fe07859412600d9119060784358
Stephen Niedzielski <stephen@niedzielski.com>
2021-06-21 21:34:05 -0600

    [build] Add version string

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import {version} from '../package.json'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,3 +16,9 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
+
+// [strings]
+console.log(`Perfect Memory v${version}
+   ┌>°┐
+by │  │idoid
+   └──┘`)
