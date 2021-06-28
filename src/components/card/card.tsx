4b373e521fe9e11c6a44eae8193540e5603a4840
Stephen Niedzielski <stephen@niedzielski.com>
2021-06-27 22:16:16 -0600

    [UI][card] Revise styles

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,7 +1,7 @@
 import type React from 'react'
 import './card.css'
 
-export type CardProps = Readonly<{children?: React.ReactNode}>
+export type CardProps = Readonly<{children: React.ReactNode}>
 
 export function Card({children}: CardProps) {
   return <div className='card'>{children}</div>
