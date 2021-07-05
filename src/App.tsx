7629647112460eb9c6ca6300d243dd5c4e0ccec1
Stephen Niedzielski <stephen@niedzielski.com>
2021-07-05 00:05:09 -0600

    [build] Patch in create-react-app's redux-typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Counter } from './features/counter/Counter';
 import './App.css';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Counter />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
