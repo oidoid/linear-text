252bbf1f964c2a3822e54569b5ec666e1464d03b
Stephen Niedzielski <stephen@niedzielski.com>
2021-07-13 00:20:21 -0600

    [toolbar][menu] Rename menu to toolbar

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -8,16 +8,16 @@ import {Disclosure} from '../disclosure/disclosure'
 import {Icon} from '../icon/icon'
 import {IconButton} from '../icon-button/icon-button'
 import {ListItem} from '../list-item/list-item'
-import {MenuCard} from '../menu/menu'
 import {t} from '@lingui/macro'
 import {TabRecord} from '../../tab/tab-record'
+import {ToolbarCard} from '../toolbar/toolbar'
 import {Trans} from '@lingui/react'
 import {UnorderedList} from '../unordered-list/unordered-list'
 import {useAppDispatch, useAppSelector} from '../../hooks/use-store'
 
 import navigationIconAddRecord from './navigation-icon-add-record.svg'
 import navigationIconRemoveRecord from './navigation-icon-remove-record.svg'
-import navigationIconToggleMenu from './navigation-icon-toggle-menu.svg'
+import navigationIconToggleToolbar from './navigation-icon-toggle-toolbar.svg'
 
 import './navigation.css'
 
@@ -47,13 +47,13 @@ function NavigationList() {
       <UnorderedList horizontal>
         <ListItem>
           <Disclosure
-            summary={<Icon alt='' src={navigationIconToggleMenu} />}
-            title={t`button-toggle-menu__title`}
+            summary={<Icon alt='' src={navigationIconToggleToolbar} />}
+            title={t`button-toggle-toolbar__title`}
           >
-            <MenuCard />
+            <ToolbarCard />
           </Disclosure>
           <label className='icon-buttonish__label'>
-            <Trans id='button-toggle-menu__label' />
+            <Trans id='button-toggle-toolbar__label' />
           </label>
         </ListItem>
         <ListItem>
--- src/components/toolbar/toolbar.tsx
+++ src/components/toolbar/toolbar.tsx
@@ -1,3 +1,5 @@
+import type {FileWithHandle} from 'browser-fs-access'
+
 import {BubbleCard} from '../bubble-card/bubble-card'
 import {
   loadTabFileAsync,
@@ -13,28 +15,27 @@ import {UnorderedList} from '../unordered-list/unordered-list'
 import {useAppSelector, useAppDispatch} from '../../hooks/use-store'
 import {useEffect, useState} from 'react'
 
-import menuIconHelp from './menu-icon-help.svg'
-import menuIconLoadFile from './menu-icon-load-file.svg'
-import menuIconNewFile from './menu-icon-new-file.svg'
-import menuIconRedo from './menu-icon-redo.svg'
-import menuIconSaveFile from './menu-icon-save-file.svg'
-import menuIconSaveFileAs from './menu-icon-save-file-as.svg'
-import menuIconUndo from './menu-icon-undo.svg'
+import toolbarIconHelp from './toolbar-icon-help.svg'
+import toolbarIconLoadFile from './toolbar-icon-load-file.svg'
+import toolbarIconNewFile from './toolbar-icon-new-file.svg'
+import toolbarIconRedo from './toolbar-icon-redo.svg'
+import toolbarIconSaveFile from './toolbar-icon-save-file.svg'
+import toolbarIconSaveFileAs from './toolbar-icon-save-file-as.svg'
+import toolbarIconUndo from './toolbar-icon-undo.svg'
 
-import './menu.css'
-import type {FileWithHandle} from 'browser-fs-access'
+import './toolbar.css'
 
-export function MenuCard() {
+export function ToolbarCard() {
   return (
-    <div className='menu-card'>
+    <div className='toolbar-card'>
       <BubbleCard>
-        <Menu />
+        <Toolbar />
       </BubbleCard>
     </div>
   )
 }
 
-export function Menu() {
+export function Toolbar() {
   const [file, setFile] = useState<FileWithHandle>()
   const records = useAppSelector(selectRecords)
   const dispatch = useAppDispatch()
@@ -45,13 +46,13 @@ export function Menu() {
   return (
     <UnorderedList>
       <ListItem>
-        <div className='menu__button-group'>
+        <div className='toolbar__button-group'>
           <IconButton
             label={t`button-undo__label`}
             onClick={() => {
               throw Error('Undo unimplemented.')
             }}
-            src={menuIconUndo}
+            src={toolbarIconUndo}
             title={t`button-undo__title`}
           />
           <IconButton
@@ -59,19 +60,19 @@ export function Menu() {
             onClick={() => {
               throw Error('Redo unimplemented.')
             }}
-            src={menuIconRedo}
+            src={toolbarIconRedo}
             title={t`button-redo__title`}
           />
         </div>
       </ListItem>
       <ListItem>
-        <div className='menu__button-group'>
+        <div className='toolbar__button-group'>
           <IconButton
             label={t`button-save-file__label`}
             onClick={() => {
               throw Error('Save unimplemented.')
             }}
-            src={menuIconSaveFile}
+            src={toolbarIconSaveFile}
             title={t`button-save-file__title`}
           />
           <IconButton
@@ -79,13 +80,13 @@ export function Menu() {
             onClick={() => {
               throw Error('Save as unimplemented.')
             }}
-            src={menuIconSaveFileAs}
+            src={toolbarIconSaveFileAs}
             title={t`button-save-file-as__title`}
           />
         </div>
       </ListItem>
       <ListItem>
-        <div className='menu__button-group'>
+        <div className='toolbar__button-group'>
           <IconButton
             label={t`button-load-file__label`}
             onClick={async () => {
@@ -93,19 +94,19 @@ export function Menu() {
               setFile(result)
               dispatch(loadTabFileAsync(result))
             }}
-            src={menuIconLoadFile}
+            src={toolbarIconLoadFile}
             title={t`button-load-file__title`}
           />
           <IconButton
             label={t`button-new-file__label`}
             onClick={() => dispatch(newFile())}
-            src={menuIconNewFile}
+            src={toolbarIconNewFile}
             title={t`button-new-file__title`}
           />
         </div>
       </ListItem>
       <ListItem>
-        <div className='menu__button-group'>
+        <div className='toolbar__button-group'>
           <IconButton
             label={t`button-help__label`}
             onClick={() => {
@@ -116,7 +117,7 @@ export function Menu() {
               dispatch(setFocus(index))
               // throw Error('Help unimplemented.')
             }}
-            src={menuIconHelp}
+            src={toolbarIconHelp}
             title={t`button-help__title`}
           />
         </div>
