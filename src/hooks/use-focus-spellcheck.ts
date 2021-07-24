1c6783bade99d42fd7bce5bb28674164c28d9ac6
Stephen Niedzielski <stephen@niedzielski.com>
2021-07-24 12:48:40 -0600

    [spellchecker][hook][bug] Fix text duplication on focus

--- src/hooks/use-focus-spellcheck.ts
+++ src/hooks/use-focus-spellcheck.ts
@@ -14,12 +14,22 @@ export function useFocusSpellchecker<
   const onBlurSpellcheck = useCallback((ev: React.FocusEvent<T>) => {
     setSpellcheck(false)
     // Invalidate spellchecker squiggles.
+    //
+    // A previous implementation had a similar invalidation on focus too but it
+    // caused the text to be duplicated 100% of the time in certain scenarios on
+    // at least Chromium:
+    //
+    // 1. Focus a note in the middle of a group.
+    // 2. Press enter to create a draft.
+    // 3. Press tab to jump to the next note and dismiss the draft.
+    //
+    // At this point, the text will be "doubled". If the text is modified, the
+    // double will be persisted into the model. If tabbed away, the text will
+    // revert to the model. The issue doesn't occur on blur and the invalidation
+    // only seems to be necessary for blurring. However, this bug indicates the
+    // blur implementation may be a timebomb.
     ev.currentTarget.setRangeText(ev.currentTarget.value)
   }, [])
-  const onFocusSpellcheck = useCallback((ev: React.FocusEvent<T>) => {
-    setSpellcheck(true)
-    // Invalidate spellchecker squiggles.
-    ev.currentTarget.setRangeText(ev.currentTarget.value)
-  }, [])
+  const onFocusSpellcheck = useCallback(() => setSpellcheck(true), [])
   return {spellcheck, onBlurSpellcheck, onFocusSpellcheck}
 }
