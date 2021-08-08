e7207f83e52df985eec6360ed1dd371967996d18
Stephen Niedzielski <stephen@niedzielski.com>
2021-08-08 16:25:23 -0600

    [file] Fix a tabular reference

--- src/table-serializer/table-serializer.ts
+++ src/table-serializer/table-serializer.ts
@@ -1,6 +1,6 @@
 import type {Table} from '../table/table'
 
-/** Serialize a table to a tab-delimited value. */
+/** Serialize a table to a newline-delimited value. */
 export function serializeTable(table: Readonly<Table>): string {
   return table.lines
     .filter(({state}) => state !== 'draft')
