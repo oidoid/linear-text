0785f246aae20cdb999e97e589d655e3746f51cf
Stephen Niedzielski <stephen@niedzielski.com>
2021-08-08 23:25:09 -0600

    [card] Add header support to card

--- src/elements/card-element/card-element.tsx
+++ src/elements/card-element/card-element.tsx
@@ -1,9 +1,20 @@
 import type {ReactNode} from 'react'
 
+import {HeaderElement, HeaderProps} from '../header-element/header-element'
+
 import './card-element.css'
 
-export type CardProps = Readonly<{children: ReactNode}>
+export type CardProps = Readonly<{
+  children: ReactNode
+  headerProps?: HeaderProps
+}>
 
-export function CardElement({children}: CardProps): JSX.Element {
-  return <div className='card'>{children}</div>
+export function CardElement({children, headerProps}: CardProps): JSX.Element {
+  const className = `card ${headerProps == null ? '' : 'card--header'}`
+  return (
+    <div className={className}>
+      {headerProps == null ? null : <HeaderElement {...headerProps} />}
+      {children}
+    </div>
+  )
 }
