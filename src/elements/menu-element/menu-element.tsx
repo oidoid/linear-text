2926614c4cd892f7c63eaf4003215e40375eb330
Stephen Niedzielski <stephen@niedzielski.com>
2021-08-15 18:00:03 -0600

    [menu] Remove menu toggle

--- src/elements/menu-element/menu-element.tsx
+++ src/elements/menu-element/menu-element.tsx
@@ -17,7 +17,6 @@ import {useCallback, useState} from 'react'
 
 import addLineIcon from '../../icons/add-line-icon.svg'
 import removeLineIcon from '../../icons/remove-line-icon.svg'
-import toggleMenuIcon from '../../icons/toggle-menu-icon.svg'
 import toggleToolbarIcon from '../../icons/toggle-toolbar-icon.svg'
 
 import './menu-element.css'
@@ -47,51 +46,44 @@ function MenuListElement(): JSX.Element {
   const toggleHelp = useCallback(() => setShowHelp(showHelp => !showHelp), [])
   return (
     <div className='menu-list'>
-      <DisclosureElement
-        accessKey={t`button-toggle-menu__access-key`}
-        summary={<IconElement alt='' src={toggleMenuIcon} />}
-        title={t`button-toggle-menu__title`}
-        open
-      >
-        <UnorderedListElement layout='horizontal'>
-          <li>
-            <DisclosureElement
-              accessKey={t`button-toggle-toolbar__access-key`}
-              summary={<IconElement alt='' src={toggleToolbarIcon} />}
-              title={t`button-toggle-toolbar__title`}
-            >
-              <ToolbarCardElement onHelpClick={toggleHelp} />
-            </DisclosureElement>
-            <label className='icon-buttonish__label'>
-              <Trans id='button-toggle-toolbar__label' />
-            </label>
-          </li>
-          <li>
-            <IconButtonElement
-              accessKey={t`button-remove-line__access-key`}
-              label={t`button-remove-line__label`}
-              onClick={ev => {
-                ev.stopPropagation()
-                if (tableState.focus == null) return
-                dispatch(
-                  removeLineAction({id: tableState.focus, nextFocus: 'prev'})
-                )
-              }}
-              src={removeLineIcon}
-              title={t`button-remove-line__title`}
-            />
-          </li>
-          <li>
-            <IconButtonElement
-              accessKey={t`button-add-line__access-key`}
-              label={t`button-add-line__label`}
-              onClick={() => dispatch(addDraftAction())}
-              src={addLineIcon}
-              title={t`button-add-line__title`}
-            />
-          </li>
-        </UnorderedListElement>
-      </DisclosureElement>
+      <UnorderedListElement layout='horizontal'>
+        <li>
+          <DisclosureElement
+            accessKey={t`button-toggle-toolbar__access-key`}
+            summary={<IconElement alt='' src={toggleToolbarIcon} />}
+            title={t`button-toggle-toolbar__title`}
+          >
+            <ToolbarCardElement onHelpClick={toggleHelp} />
+          </DisclosureElement>
+          <label className='icon-buttonish__label'>
+            <Trans id='button-toggle-toolbar__label' />
+          </label>
+        </li>
+        <li>
+          <IconButtonElement
+            accessKey={t`button-remove-line__access-key`}
+            label={t`button-remove-line__label`}
+            onClick={ev => {
+              ev.stopPropagation()
+              if (tableState.focus == null) return
+              dispatch(
+                removeLineAction({id: tableState.focus, nextFocus: 'prev'})
+              )
+            }}
+            src={removeLineIcon}
+            title={t`button-remove-line__title`}
+          />
+        </li>
+        <li>
+          <IconButtonElement
+            accessKey={t`button-add-line__access-key`}
+            label={t`button-add-line__label`}
+            onClick={() => dispatch(addDraftAction())}
+            src={addLineIcon}
+            title={t`button-add-line__title`}
+          />
+        </li>
+      </UnorderedListElement>
       {showHelp && <HelpDialogCardElement onDismissClick={toggleHelp} />}
     </div>
   )
