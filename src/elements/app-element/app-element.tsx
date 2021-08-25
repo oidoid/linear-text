d888d1cb09404240e42f94d1321e288505400c3e
Stephen Niedzielski <stephen@niedzielski.com>
2021-08-24 22:58:51 -0600

    [app] Extract functions from component

--- src/elements/app-element/app-element.tsx
+++ src/elements/app-element/app-element.tsx
@@ -12,20 +12,8 @@ export type AppProps = Readonly<{'data-testid'?: string}>
 export function AppElement({'data-testid': testID}: AppProps): JSX.Element {
   const tableState = useAppSelector(selectTableState)
   useEffect(() => {
-    const filename = tableState.filename
-    document.title = tableState.invalidated
-      ? filename == null
-        ? t`app-title-with-unsaved`
-        : t`app-title-with-unsaved-${filename}`
-      : filename == null
-      ? t`app-title-with-saved`
-      : t`app-title-with-saved-${filename}`
-
-    const favicon = getFavicon()
-    if (!favicon) return
-    favicon.href = `${process.env.PUBLIC_URL}/favicon${
-      tableState.invalidated ? '-unsaved' : ''
-    }.svg`
+    setTitle(tableState.filename, tableState.invalidated)
+    setFavicon(tableState.invalidated)
   }, [tableState.filename, tableState.invalidated])
   return (
     <div className='app' data-testid={testID}>
@@ -39,6 +27,21 @@ export function AppElement({'data-testid': testID}: AppProps): JSX.Element {
   )
 }
 
-function getFavicon(): HTMLLinkElement | null {
-  return document.querySelector('link[rel="icon"]')
+function setTitle(filename: string | undefined, invalidated: boolean): void {
+  document.title = invalidated
+    ? filename == null
+      ? t`app-title-with-unsaved`
+      : t`app-title-with-unsaved-${filename}`
+    : filename == null
+    ? t`app-title-with-saved`
+    : t`app-title-with-saved-${filename}`
+}
+
+function setFavicon(invalidated: boolean): void {
+  const favicon: HTMLLinkElement | null =
+    document.querySelector('link[rel="icon"]')
+  if (favicon == null) return
+  favicon.href = `${process.env.PUBLIC_URL}/favicon${
+    invalidated ? '-unsaved' : ''
+  }.svg`
 }
