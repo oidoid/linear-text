cfda3bd3b62419ba51eb1e95ffa3d05da367dd8b
Stephen Niedzielski <stephen@niedzielski.com>
2021-10-10 01:37:29 -0600

    [text-type][image] Add image line type recognition

--- src/table-parser/text-type.ts
+++ src/table-parser/text-type.ts
@@ -2,9 +2,12 @@ import {isNumeric} from '../utils/string-util'
 
 /** String-encoded data type of the text value. */
 export type TextType =
+  /** URL or URI. Not a known image. */
   | 'uri-data'
   | 'uri-http'
   | 'uri-file'
+  /** An image URI or filename. Takes precedence over other URI line types. */
+  | 'image'
   /** Nonempty string. */
   | 'text'
   /** Finite number. */
@@ -23,8 +26,40 @@ export function parseTextType(text: string): TextType {
   const trimmed = text.trim()
   if (/^(true|false)$/i.test(trimmed)) return 'boolean'
   if (isNumeric(trimmed)) return 'number'
+  if (endsWithImageExtension(trimmed) || startsWithDataImageMIME(trimmed))
+    return 'image'
   if (/^file:\/\//.test(trimmed)) return 'uri-file'
   if (/^https?:\/\//.test(trimmed)) return 'uri-http'
   if (/^data:/.test(trimmed)) return 'uri-data'
   return 'text'
+}
+
+// https://developer.mozilla.org/en-US/docs/Web/Media/Formats/Image_types
+const imageExtensions: readonly string[] = Object.freeze([
+  'apng',
+  'avif',
+  'gif',
+  'jpg',
+  'jpeg',
+  'jfif',
+  'pjpeg',
+  'pjp',
+  'png',
+  'svg',
+  'webp',
+  'bmp',
+  'ico',
+  'cur',
+  'tif',
+  'tiff'
+])
+
+function endsWithImageExtension(text: string): boolean {
+  return imageExtensions.some(extension =>
+    new RegExp(`.\\.${extension}$`).test(text)
+  )
+}
+
+function startsWithDataImageMIME(text: string): boolean {
+  return text.startsWith('data:image/')
 }
